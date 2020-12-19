9ab136606c7361ac14be74256168bc9b504321bd
Maksim <pegashev95@gmail.com>
2020-12-19 21:53:56 +0200

    123

---
+++ Assets/js/example.js
@@ -0,0 +1,16 @@
+let num = 20;
+
+switch (num) {
+    case num < 19:
+        console.log("Мало!");
+        break;
+    case num > 20:
+        console.log("Много!");
+        break;
+    case 20:
+        console.log("В самый раз!");
+        break;
+    default:
+        console.log("Что-то пошло не так!");
+        break;
+}
